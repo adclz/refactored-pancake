2a30519f7ff8cc73dae1238155632a03b83fdd2c
adclz <clauzeladrien@gmail.com>
2024-06-11 14:55:37 +0200

    fix: back to original beremiz xslt files

--- packages/core/esbuild.config.mjs
+++ packages/core/esbuild.config.mjs
@@ -5,19 +5,27 @@ import child_process from "node:child_process"
 import findNodeModules from "find-node-modules"
 
 const modulesDir = findNodeModules()
+const tmp = path.join(process.cwd(), modulesDir[0], "/tmp")
+if (!fs.existsSync(tmp)) fs.mkdirSync(tmp)
 
 // Replace the content of a xslt file into a saxon SEF schema
 // xslt3 does not have to be installed globally
 const xsltToSef = (id) => {
-  const xslt3Path = path.join(modulesDir[0], "/xslt3/xslt3.js")
-  const sefJsonPath = id.replace(".xslt", ".sef.json");
-  child_process.spawnSync("node", [`${xslt3Path}`, `-xsl:${id}`, `-export:${sefJsonPath}`], { cwd: process.cwd() })
-    
-  const code = fs.readFileSync(sefJsonPath, 'utf8')
-      .replace("xmlns:ns=\"beremiz\"", "xmlns:ns=\"http://saxonica.com/ns/globalJS\"")
-      .replace("version=\"1.0\"", "version=\"3.0\"")
-  fs.rmSync(sefJsonPath)
-  return code
+    const xslt3Path = path.join(process.cwd(), modulesDir[0], "/xslt3/xslt3.js")
+
+    const tmpFile = path.join(tmp, path.basename(id))
+    const tmpCode = fs.readFileSync(id, "utf-8")
+        .replace('xmlns:ns="beremiz"', 'xmlns:ns="http://saxonica.com/ns/globalJS"')
+        .replace('version="1.0"', 'version="3.0"')
+
+    fs.writeFileSync(tmpFile, tmpCode)
+
+    const sefJsonPath = id.replace(".xslt", ".sef.json");
+    child_process.spawnSync("node", [`${xslt3Path}`, `-xsl:${tmpFile}`, `-export:${sefJsonPath}`])
+
+    const code = fs.readFileSync(sefJsonPath, 'utf8')
+    fs.rmSync(sefJsonPath)
+    return code
 }
 
 export const esbuildOptions = {
--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -6,18 +6,25 @@ import child_process from "node:child_process"
 import findNodeModules from "find-node-modules"
 
 const modulesDir = findNodeModules()
-
+const tmp = path.join(process.cwd(), modulesDir[0], "/tmp")
+if (!fs.existsSync(tmp)) fs.mkdirSync(tmp)
 
 // Replace the content of a xslt file into a saxon SEF schema
 // xslt3 does not have to be installed globally
 const xsltToSef = (id: string) => {
   const xslt3Path = path.join(process.cwd(), modulesDir[0], "/xslt3/xslt3.js")
-  const sefJsonPath = id.replace(".xslt", ".sef.json");
-  child_process.spawnSync("node", [`${xslt3Path}`, `-xsl:${id}`, `-export:${sefJsonPath}`])
   
+  const tmpFile = path.join(tmp, path.basename(id))
+  const tmpCode = fs.readFileSync(id, "utf-8")
+      .replace('xmlns:ns="beremiz"', 'xmlns:ns="http://saxonica.com/ns/globalJS"')
+      .replace('version="1.0"', 'version="3.0"')
+  
+  fs.writeFileSync(tmpFile, tmpCode)
+  
+  const sefJsonPath = id.replace(".xslt", ".sef.json");
+  child_process.spawnSync("node", [`${xslt3Path}`, `-xsl:${tmpFile}`, `-export:${sefJsonPath}`])
+
   const code = fs.readFileSync(sefJsonPath, 'utf8')
-      .replace("xmlns:ns=\"beremiz\"", "xmlns:ns=\"http://saxonica.com/ns/globalJS\"")
-      .replace("version=\"1.0\"", "version=\"3.0\"")
   fs.rmSync(sefJsonPath)
   return code
 }
